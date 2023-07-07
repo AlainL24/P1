6ec3d66b999abe09b76802f2e5cb91d6aed5cf44
AlainL24 <136858608+AlainL24@users.noreply.github.com>
2023-07-07 14:13:03 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const openBtn = document.getElementById('howToPlayModal');
+
+const modal = document.getElementById('modal');
+
+const close = document.getElementById('close');
+
+const hiddenWord = document.getElementById('hiddenWord');
